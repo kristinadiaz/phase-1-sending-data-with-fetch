37181896ed63131d1ffb83e8ff920ddb87ffa0cb
Kristina Diaz <kristina.diaz84@gmail.com>
2022-02-23 18:05:33 -0700

    commit tree 8ca49a25927d7dc3b4e4f8caf65a19f2d079487f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    })
+    .then(res => res.json())
+    .then(user => appendId(user))
+    .catch(error => errorMessage(error))
+}
+
+function appendId(user) {
+    let p = document.createElement('p')
+    p.textContent = user.id 
+    document.querySelector('body').appendChild(p)
+}
+
+function errorMessage(error) {
+    document.body.innerHTML = error.message
+}
